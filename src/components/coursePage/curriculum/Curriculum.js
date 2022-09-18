a1f38bfcd266d25b242d927a6accf167cdb58103
Tarek Gamal <TarekMGamal1@gmail.com>
2022-09-18 17:31:58 +0200

    feat: Add content section

--- src/components/coursePage/curriculum/Curriculum.js
+++ src/components/coursePage/curriculum/Curriculum.js
@@ -10,7 +10,13 @@ function Curriculum() {
   let content = courseData.content;
   let content1 = [],
     content2 = [];
+
+  let cnt = 0;
   for (let i in content) {
+    let section = content[i];
+    let lectures = section["lectures"];
+    cnt += lectures.length;
+
     if (i < content.length / 2) {
       content1.push(content[i]);
     } else {
@@ -19,40 +25,58 @@ function Curriculum() {
   }
 
   const [open, setOpen] = useState(false);
+  const [expandAll, setExpandAll] = useState(false);
 
   if (courseData === undefined) {
     return <div>Loading</div>;
   } else {
     return (
       <>
-        <div>
-          <div className="contentContainer">
-            {content1.map((element) => {
-              return (
-                <div>
-                  <CurriculumItem data={element} />
-                </div>
-              );
-            })}
-            <Collapse in={open}>
-              <div className="contentContainer">
-                {content2.map((element) => {
-                  return (
-                    <div>
-                      <CurriculumItem data={element} />
-                    </div>
-                  );
-                })}
-              </div>
-            </Collapse>
-            <button
-              onClick={() => {
-                setOpen(!open);
-              }}
-            >
-              {open ? "show less" : "show more"}
-            </button>
+        <h4 className="courseContentHeader">Course content</h4>
+        <div className="cont">
+          <div className="left">
+            {content.length} sections • {cnt} lectures
           </div>
+          <button
+            className="expandAllButton"
+            onClick={() => {
+              setExpandAll(!expandAll);
+            }}
+          >
+            Expand all sections
+          </button>
+        </div>
+        <div className="contentContainer">
+          {content1.map((element) => {
+            return (
+              <div>
+                <CurriculumItem
+                  className="section"
+                  data={element}
+                  expandState={expandAll}
+                />
+              </div>
+            );
+          })}
+          <Collapse in={open}>
+            <div>
+              {content2.map((element) => {
+                return (
+                  <div className="sectionWrapper">
+                    <CurriculumItem data={element} expandState={expandAll} />
+                  </div>
+                );
+              })}
+            </div>
+          </Collapse>
+          <button
+            className="showMoreButton"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            {open ? "show less" : "show more"}
+          </button>
         </div>
       </>
     );
--- src/components/coursePage/curriculum/curriculumItem/CurricilumItem.js
+++ src/components/coursePage/curriculum/curriculumItem/CurricilumItem.js
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import Collapse from "react-bootstrap/Collapse";
-
-function CurriculumItem({ data }) {
+import "./CurricilumItem.css";
+import { PlayCircleFill, ChevronDown, ChevronUp } from "react-bootstrap-icons";
+function CurriculumItem({ data, expandState }) {
   const [openLectures, setOpenLectures] = useState(false);
   let lectures = data.lectures;
+  console.log(expandState);
 
   return (
     <>
       <button
+        className="section"
         onClick={() => {
           setOpenLectures(!openLectures);
         }}
       >
-        {data.section}
+        <div className="icon">
+          {openLectures ? <ChevronUp /> : <ChevronDown />}
+        </div>
+        <div>{data.section}</div>
+        <div className="right">{lectures.length} lectures</div>
       </button>
-      <Collapse in={openLectures}>
-        <div>
+      <Collapse in={openLectures || expandState}>
+        <div className="expandedSection">
           {lectures.map((element) => {
-            return <div>{element}</div>;
+            return (
+              <div>
+                <PlayCircleFill /> {element}
+              </div>
+            );
           })}
         </div>
       </Collapse>
